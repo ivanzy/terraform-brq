b1ee6a6e91cf7e62cf53f04e02f0073ecada57a5
ivan <idimitryz@gmail>
2022-02-03 19:12:10 +0100

    adicionei subrede e associçao entre rotas e subrede

--- helloworld.tf
+++ helloworld.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  #access_key = "" 
-  #secret_key = ""
+  access_key = "AKIA4BJ3JJCBJEGPDL67" 
+  secret_key = "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"
 }
 
 resource "aws_vpc" "vpc_brq" {
@@ -45,14 +45,19 @@ resource "aws_route_table" "rotas_brq" {
   }
 }
 
-# resource "aws_subnet" "subrede_brq" {
-#   vpc_id     = aws_vpc.vpc_brq.id
-#   cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "subrede_brq" {
+  vpc_id     = aws_vpc.vpc_brq.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "RonyRustico"
+  }
+}
 
-#   tags = {
-#     Name = "RonyRustico"
-#   }
-# }
+resource "aws_route_table_association" "associacao" {
+  subnet_id      =  aws_subnet.subrede_brq.id
+  route_table_id = aws_route_table.rotas_brq.id
+}
 
 # resource "aws_instance" "ola-mundo" {
 #     ami = "ami-04505e74c0741db8d"
