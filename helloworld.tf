bb3faf3eebd43ac182e2a798b60ebc73925df66d
ivan <idimitryz@gmail>
2022-02-03 19:43:08 +0100

    criei um grupo de segurança

--- helloworld.tf
+++ helloworld.tf
@@ -9,14 +9,14 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA4BJ3JJCBJEGPDL67" 
+  access_key = "AKIA4BJ3JJCBJEGPDL67"
   secret_key = "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"
 }
 
 resource "aws_vpc" "vpc_brq" {
   cidr_block = "10.0.0.0/16"
   tags = {
-      Name = "VPC_legal"
+    Name = "VPC_legal"
   }
 }
 
@@ -36,8 +36,8 @@ resource "aws_route_table" "rotas_brq" {
   }
 
   route {
-    ipv6_cidr_block        = "::/0"
-    gateway_id = aws_internet_gateway.gw_brq.id
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw_brq.id
   }
 
   tags = {
@@ -46,8 +46,8 @@ resource "aws_route_table" "rotas_brq" {
 }
 
 resource "aws_subnet" "subrede_brq" {
-  vpc_id     = aws_vpc.vpc_brq.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.vpc_brq.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "RonyRustico"
@@ -55,14 +55,57 @@ resource "aws_subnet" "subrede_brq" {
 }
 
 resource "aws_route_table_association" "associacao" {
-  subnet_id      =  aws_subnet.subrede_brq.id
+  subnet_id      = aws_subnet.subrede_brq.id
   route_table_id = aws_route_table.rotas_brq.id
 }
+
+resource "aws_security_group" "firewall" {
+  name        = "abrir_portas"
+  description = "Abrir porta 22 (SSH), 443 (HTTPS) e 80 (HTTP)"
+  vpc_id      = aws_vpc.vpc_brq.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Dudu"
+  }
+}
+
 
 # resource "aws_instance" "ola-mundo" {
 #     ami = "ami-04505e74c0741db8d"
 #     instance_type = "t2.micro"
-    
+
 #     tags = {
 #         Name = "Robson"
 #     }
